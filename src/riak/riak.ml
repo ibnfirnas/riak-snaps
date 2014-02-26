3c4272a7529325a36feda1ba2afdb1e29cf8002a
Siraaj Khandkar <siraaj@khandkar.net>
2014-02-26 18:25:15 -0500

    Factor-out key fetching.

--- src/riak/riak.ml
+++ src/riak/riak.ml
@@ -10,17 +10,18 @@ let make ?(hostname="localhost") ?(port=8098) () =
   ; port
   }
 
-let fetch_keys_2i {hostname; port} ~bucket =
-  let uri = sprintf "http://%s:%d/buckets/%s/index/bucket_bin/%s" hostname port bucket bucket in
+let fetch_keys ~uri =
   let data = Shell.out ~prog:"curl" ~args:[uri] in
   let json = Ezjsonm.from_string data in
   Ezjsonm.(get_list get_string (find json ["keys"]))
 
+let fetch_keys_2i {hostname; port} ~bucket =
+  let uri = sprintf "http://%s:%d/buckets/%s/index/bucket_bin/%s" hostname port bucket bucket in
+  fetch_keys ~uri
+
 let fetch_keys_brutally {hostname; port} ~bucket =
   let uri = sprintf "http://%s:%d/riak/%s?keys=true" hostname port bucket in
-  let data = Shell.out ~prog:"curl" ~args:[uri] in
-  let json = Ezjsonm.from_string data in
-  Ezjsonm.(get_list get_string (find json ["keys"]))
+  fetch_keys ~uri
 
 let fetch_value {hostname; port} ~bucket key =
   let uri = sprintf "http://%s:%d/riak/%s/%s" hostname port bucket key in
