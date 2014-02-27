d73f4a4877aea4af6e03c815d3d28a792e9f0a7a
Siraaj Khandkar <siraaj@khandkar.net>
2014-02-27 17:23:33 -0500

    Use Async.Command to drive the program.

--- src/snaps/snaps_main.ml
+++ src/snaps/snaps_main.ml
@@ -3,38 +3,31 @@ open Async.Std
 
 open Snaps_pervasives
 
-type options =
-  { repo_path : string
-  ; hostname  : string
-  ; bucket    : string
-  }
-
-let take_snapshots ~repo_path ~hostname ~bucket =
+let main ~repo_path ~hostname ~port ~bucket =
   let db = Snaps_db.create ~path:repo_path in
-  let riak = Riak.make ~hostname () in
+  let riak = Riak.make ~hostname ~port () in
   let keys = Riak.fetch_keys_2i riak ~bucket in
   let fetch = Riak.fetch_value riak ~bucket in
   let store = Snaps_db.put db ~bucket in
-  List.iter keys ~f:(fetch |- store)
+  Deferred.List.iter keys ~f:(fetch |- store |- return) ~how:`Parallel
 
-let get_opts () =
-    try Ok { repo_path = Sys.argv.(1)
-           ; hostname  = Sys.argv.(2)
-           ; bucket    = Sys.argv.(3)
-           }
-    with Invalid_argument "index out of bounds" ->
-      Error `Invalid_argument
+let () =
+  Command.async_basic
+    ~summary:"Snapshot Riak objects to a Git repository."
+    Command.Spec.(
+      empty
 
-let main () =
-  begin match get_opts () with
-  | Ok {repo_path; hostname; bucket} ->
-    take_snapshots ~repo_path ~hostname ~bucket;
-    shutdown 0;
+      +> flag "-repo-path" (required string)
+        ~doc:" Path to directory in which to store data"
 
-  | Error `Invalid_argument ->
-    eprintf "USAGE: %s repo_path hostname bucket\n%!" Sys.argv.(0);
-    shutdown 1
-  end;
-  never_returns (Scheduler.go ())
+      +> flag "-host" (optional_with_default "localhost" string)
+        ~doc:" Riak hostname or IP address (default: localhost)"
 
-let () = main ()
+      +> flag "-port" (optional_with_default 8098 int)
+        ~doc:" Riak HTTP port (default: 8098)"
+
+      +> flag "-bucket" (required string)
+        ~doc:" Riak bucket to take snapshots from"
+    )
+    (fun repo_path hostname port bucket () -> main ~repo_path ~hostname ~port ~bucket)
+  |> Command.run
