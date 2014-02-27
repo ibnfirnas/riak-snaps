20a3202e0adb9c5373687588dce713ee3549c873
Siraaj Khandkar <siraaj@khandkar.net>
2014-02-26 21:41:59 -0500

    Make log statements shorter and aligned.

--- src/snaps/snaps_db.ml
+++ src/snaps/snaps_db.ml
@@ -15,20 +15,20 @@ let put {path} ~bucket (key, value) =
   Shell.cd path;
   Shell.mkdir bucket;
   let filepath = Filename.concat bucket key in
-  eprintf "Writing: %S\n%!" filepath;
+  eprintf "Write  : %S\n%!" filepath;
   Out_channel.write_all filepath ~data:value;
   Git.add ~filepath;
   match Git.status ~filepath with
   | Git.Added ->
-    eprintf "Committing: %S. Known status: Added\n%!" filepath;
+    eprintf "Commit : %S. Known status: Added\n%!" filepath;
     Git.commit ~msg:(sprintf "'Add %s'" filepath)
 
   | Git.Modified ->
-    eprintf "Committing: %S. Known status: Modified\n%!" filepath;
+    eprintf "Commit : %S. Known status: Modified\n%!" filepath;
     Git.commit ~msg:(sprintf "'Update %s'" filepath)
 
   | Git.Unchanged ->
-    eprintf "Skipping: %S. Known status: Unchanged\n%!" filepath
+    eprintf "Skip   : %S. Known status: Unchanged\n%!" filepath
 
   | Git.Unexpected status ->
-    eprintf "Skipping: %S. Unknown status: %S\n%!" filepath status
+    eprintf "Skip   : %S. Unknown status: %S\n%!" filepath status
