b9772c2e4712c76ff1c19481de04fe22af5421b0
Siraaj Khandkar <siraaj@khandkar.net>
2014-02-28 21:05:27 -0500

    Fetch and store asynchronously.

--- src/snaps/snaps_main.ml
+++ src/snaps/snaps_main.ml
@@ -3,13 +3,34 @@ open Async.Std
 
 open Snaps_pervasives
 
+let fetcher ~writer:w ~riak ~bucket =
+  let keys = Riak.fetch_keys_2i riak ~bucket in
+  let fetch key =
+    let kv = Riak.fetch_value riak ~bucket key in
+    Pipe.write w kv
+  in
+  Deferred.List.iter keys ~f:fetch ~how:`Parallel >>= fun () ->
+  Pipe.close w;
+  return ()
+
+let storer ~reader:r ~db ~bucket =
+  let rec store () =
+      Pipe.read r >>= function
+      | `Eof   -> return ()
+      | `Ok kv ->
+        Snaps_db.put db ~bucket kv;
+        store ()
+  in
+  store ()
+
 let main ~repo_path ~hostname ~port ~bucket =
   let db = Snaps_db.create ~path:repo_path in
   let riak = Riak.make ~hostname ~port () in
-  let keys = Riak.fetch_keys_2i riak ~bucket in
-  let fetch = Riak.fetch_value riak ~bucket in
-  let store = Snaps_db.put db ~bucket in
-  Deferred.List.iter keys ~f:(fetch |- store |- return) ~how:`Parallel
+  let r, w = Pipe.create () in
+  don't_wait_for (fetcher ~writer:w ~riak ~bucket);
+                  storer  ~reader:r ~db   ~bucket >>= fun () ->
+  shutdown 0;
+  return ()
 
 let () =
   Command.async_basic
@@ -29,5 +50,7 @@ let () =
       +> flag "-bucket" (required string)
         ~doc:" Riak bucket to take snapshots from"
     )
-    (fun repo_path hostname port bucket () -> main ~repo_path ~hostname ~port ~bucket)
+    ( fun repo_path hostname port bucket () ->
+        main ~repo_path ~hostname ~port ~bucket
+    )
   |> Command.run
