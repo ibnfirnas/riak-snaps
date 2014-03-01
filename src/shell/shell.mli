38d0d61e5fdbe2d328da0c8eb00db231a3fe4075
Siraaj Khandkar <siraaj@khandkar.net>
2014-03-01 15:18:51 -0500

    Remove obsolete modules: Shell, Process.

--- src/shell/shell.mli
+++
@@ -1,9 +0,0 @@
-val out : prog:string -> args:string list -> string
-
-val exe : prog:string -> args:string list -> unit
-
-val mkdir : string -> unit
-
-val cd : string -> unit
-
-val pwd : unit -> string
