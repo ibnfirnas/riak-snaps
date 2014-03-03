d2712e6048d6165fbe4f2a1f4aa535277cc9d522
Siraaj Khandkar <siraaj@khandkar.net>
2014-03-03 00:10:47 -0500

    Revise storage layout to keep number of directories low.
    Fixes #16

--- src/snaps/snaps_object_info.ml
+++ src/snaps/snaps_object_info.ml
@@ -6,8 +6,8 @@ let of_riak_obj ro =
   let ro_id  = ro.Riak.Object.id in
   let bucket = ro_id.Riak.Object.ID.bucket in
   let key    = ro_id.Riak.Object.ID.key in
-  let basedir = Filename.concat bucket key in
-  { path_to_data = Filename.concat basedir "data"
+  let (/) = Filename.concat in
+  { path_to_data = "objects" / bucket / key
   }
 
 let path_to_data {path_to_data} =
