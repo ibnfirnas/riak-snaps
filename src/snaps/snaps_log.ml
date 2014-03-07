492133e45a39e31ab684d8d8b59ae1a13bf4d188
Siraaj Khandkar <siraaj@khandkar.net>
2014-03-07 18:18:44 -0500

    Ensure log is initialized with an absolute path.

--- src/snaps/snaps_log.ml
+++ src/snaps/snaps_log.ml
@@ -16,9 +16,19 @@ module Make (Caller : CALLER) = struct
 end
 
 module Shell = Core_extended.Shell
+module Sys   = Core.Std.Sys
+
+let absolute_of_path p =
+  let cwd = Sys.getcwd () in
+  Sys.chdir p;
+  let absolute = Sys.getcwd () in
+  Sys.chdir cwd;
+  absolute
 
 let init ~level ~repo_path =
   let (/) = Filename.concat in
+  Shell.mkdir ~p:() repo_path;
+  let repo_path = absolute_of_path repo_path in
   let log_dir = repo_path / "log" in
   Shell.mkdir ~p:() log_dir;
   let filename = log_dir / "snaps.log" in
